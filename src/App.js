c230c1a1b5973a929d31220a2f3efd48623583c0
oliversisson <oliversisson@gmail.com>
2024-05-09 09:19:08 +0100

    Pre-context.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Transaction from './Transaction.js';
+
+function gaussianRandom(mean=6, stdev=1) {
+    const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+    const v = Math.random();
+    const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
+}
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTransactions((prevTs) => {
+        const newTs = [...prevTs];
+        newTs.push({key: newTs.length, fee: gaussianRandom()});
+        return newTs;
+      });
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h2>
+          Bitcoin Play!
+        </h2>
+      </div>
+      <div className="transactions">
+        <p>Available uncommitted transactions:</p>
+        {transactions.map(t => (
+          <Transaction t={t} key={t.key} />
+        ))}
+      </div>
+      <div className="newblock">
+        <p>Your proposed new block</p>
+        {transactions.filter(t => (!!t.selected && true)).map(t => (
+          <Transaction t={t} key={t.key} />
+        ))}
+        <p> Nonce: <input type="number" /> </p>
+        <button type="button">Calculate SHA256</button>
+      </div>
     </div>
   );
 }
---
+++ src/Transaction.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Transaction extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const {t} = this.props;
+    this.state = {                           
+      selected: !!t.selected,
+    }
+  }
+  onClick = () => {
+    const {t} = this.props;
+    t.selected = !t.selected;
+    this.setState({selected: t.selected});
+  }
+  render() {
+  const {t} = this.props;
+    const style = t.selected ? {color: 'red', fontWeight: 'bold'} : {};
+    return (
+//    <React.Fragment>
+      <div onClick={this.onClick}>
+        <p style={style}>Fee: {t.fee}</p>
+      </div>
+    );
+  }
+}
+
+export default Transaction;
